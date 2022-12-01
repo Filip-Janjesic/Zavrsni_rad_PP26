09bdaef667ac3a129ede3206d6fb6382cfc9f997
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-12-02 00:02:18 +0100

    sql gotov - za sada

--- sql/webshop.sql
+++ sql/webshop.sql
@@ -1,3 +1,5 @@
+# c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 <C:\korisniks\Filip\Documents\Edunova-2022\Zavrsni_rad_PP26\sql\webshop.sql
+
 drop database if exists webshop;
 create database webshop default character set utf8mb4;
 use webshop;
@@ -8,7 +10,8 @@ create table proizvod (
     naziv varchar(50) not null,
     proizvodac varchar(255) not null,
     cijena decimal(18.2) not null,
-    slika varchar(255) not null
+    slika varchar(255) not null,
+    kategorija int not null 
 
 );
 
@@ -30,7 +33,7 @@ create table narudzba (
 
 );
 
-create table categories(
+create table kategorija(
 
 	sifra int primary key auto_increment not null,
 	naziv varchar(50)
@@ -68,6 +71,18 @@ create table dijaprojekcija(
 
 	sifra int primary key auto_increment not null,
 	slika varchar(255) not null,
-	vidljivo varchar (1) 
+	vsifraljivo varchar (1) 
 
 );
+
+alter table kupljeno add foreign key (narudzba) references narudzba(sifra);
+alter table kupljeno add foreign key (proizvod) references proizvod(sifra);
+
+alter table proizvod add foreign key (kategorija) references kategorija(sifra);
+alter table narudzba add foreign key (korisnik) references korisnik(sifra);
+
+alter table komentar add foreign key (proizvod) references proizvod(sifra);
+alter table komentar add foreign key (korisnik) references korisnik(sifra) on delete cascade;
+
+alter table ocjena add foreign key (proizvod) references proizvod(sifra);
+alter table ocjena add foreign key (korisnik) references korisnik(sifra) on delete cascade;
