58d78908fd29ba0f05d03fbf5f45ebd3a0f40692
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-12-09 19:55:03 +0100

    popravak završnog rada i brisanje slike

--- sql/webshop.sql
+++ sql/webshop.sql
@@ -1,4 +1,4 @@
-# c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 <C:\korisniks\Filip\Documents\Edunova-2022\Zavrsni_rad_PP26\sql\webshop.sql
+# c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 <C:\Users\Filip\Documents\Edunova-2022\Zavrsni_rad_PP26\sql\webshop.sql
 
 drop database if exists webshop;
 create database webshop default character set utf8mb4;
@@ -25,21 +25,6 @@ create table korisnik (
 
 );
 
-create table narudzba (
-
-    sifra int primary key auto_increment not null,
-    status varchar(50) not null,
-    korisnik int
-
-);
-
-create table kategorija(
-
-	sifra int primary key auto_increment not null,
-	naziv varchar(50)
-
-);
-
 create table komentar(
 
 	sifra int primary key auto_increment not null,
@@ -58,31 +43,8 @@ create table ocjena(
 
 );
 
-create table kupljeno(
-
-	sifra int primary key auto_increment not null,
-	narudzba int,
-    proizvod int,
-    cijena decimal(18,2)
-
-);
-
-create table dijaprojekcija(
-
-	sifra int primary key auto_increment not null,
-	slika varchar(255) not null,
-	vidljivo varchar (1) 
-
-);
-
-alter table kupljeno add foreign key (narudzba) references narudzba(sifra);
-alter table kupljeno add foreign key (proizvod) references proizvod(sifra);
-
-alter table proizvod add foreign key (kategorija) references kategorija(sifra);
-alter table narudzba add foreign key (korisnik) references korisnik(sifra);
-
 alter table komentar add foreign key (proizvod) references proizvod(sifra);
 alter table komentar add foreign key (korisnik) references korisnik(sifra) on delete cascade;
-
 alter table ocjena add foreign key (proizvod) references proizvod(sifra);
 alter table ocjena add foreign key (korisnik) references korisnik(sifra) on delete cascade;
+
